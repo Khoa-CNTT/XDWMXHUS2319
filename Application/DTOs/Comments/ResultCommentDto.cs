b89c79d806dec93b6ed4dda8b029ad09a062fe41
tairomq2 <nguyenthanhtai120703@gmail.com>
2025-03-22 11:44:01 +0700

    Fix warning

---
+++ Application/DTOs/Comments/ResultCommentDto.cs
@@ -0,0 +1,20 @@
+﻿using Application.DTOs.Post;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTOs.Comments
+{
+    public class ResultCommentDto
+    {
+        public Guid CommentId { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+         public string? Content { get; set; }
+         public required string FullName { get; set; }
+            public string? ProfilePicture { get; set; }
+        }
+    }
+
