62e20c25415184060d5dfe7e033c2d28c8eddd26
Ngo_Nhat_Huy <ngonhathuy501@gmail.com>
2025-04-02 22:28:00 +0700

    FE create ridepost

--- FE/universitysharing/src/stores/action/ridePostAction.js
+++ FE/universitysharing/src/stores/action/ridePostAction.js
@@ -8,12 +8,34 @@ const token = localStorage.getItem("token");
 export const createPost = createAsyncThunk(
   "ride/createPost",
   async (
-    { content, startLocation, endLocation, startTime, postType },
+    { startLocation, endLocation, startTime, postType },
     { rejectWithValue }
   ) => {
     try {
+      const config = {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      };
+
+      const response = await axios.post(
+        "https://localhost:7053/api/ridepost/create",
+        {
+          startLocation,
+          endLocation,
+          startTime,
+          postType,
+        },
+        config
+      );
+
+      toast.success(response.data.message || "Tạo bài đăng thành công!");
+      return response.data.data; // Trả về toàn bộ response để reducer xử lý
     } catch (error) {
-      return rejectWithValue(error.response?.data || "Có lỗi xảy ra");
+      const errorMessage = error.response?.data?.message || "Có lỗi xảy ra";
+      toast.error(errorMessage);
+      return rejectWithValue(errorMessage);
     }
   }
 );
